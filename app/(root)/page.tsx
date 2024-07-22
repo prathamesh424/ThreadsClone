fe265b73851ec986ad7ee3cff836d46486044def
Prathamesh Gursal <prathmeshgursal3110@gmail.com>
2024-07-22 22:21:37 +0530

    vv

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -13,7 +13,7 @@ async function Home({
   searchParams: { [key: string]: string | undefined };
 }) {
   const user = await currentUser();
-  if (!user) redirect("/sign-in");
+  if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
