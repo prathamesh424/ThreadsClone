f35a9708d458bbfaff252e3f0e296c8fc947ca4f
Prathamesh Gursal <prathmeshgursal3110@gmail.com>
2024-10-22 13:54:20 +0530

    threads yab updated

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -29,7 +29,7 @@ const ThreadsTab = async ({ currentUserId,accountId,accountType} : Props) => {
         {
             result.threads.map((thread : any) => (
                 <ThreadCard 
-                 key={thread._id}
+                key={thread._id}
                 id= {thread._id}
                 currentUserId ={currentUserId || ""}  
                 parentId= {thread.parentId}
@@ -39,7 +39,8 @@ const ThreadsTab = async ({ currentUserId,accountId,accountType} : Props) => {
                         :{name: thread.author.name , image: thread.author.image , id: thread.author.id}}
                 community = {thread.community}
                 createdAt = {thread.createdAt}
-                comments= {thread.children}  />
+                    comments={thread.children}
+                likes={thread.likes}/>
             )
         )
         }
